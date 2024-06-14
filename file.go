e3407df538ad8e557b77f10f66dea71967056334
Johnny Steenbergen <johnny.steenbergen@crowdstrike.com>
2024-06-14 18:40:40 -0500

    chore: add File normalizer and utilize in runner

--- file.go
+++ file.go
@@ -5,13 +5,22 @@ import (
 	"encoding/json"
 	"errors"
 	"io"
+	"mime"
+	"path/filepath"
 	"strings"
 	"sync"
 	"sync/atomic"
+	"time"
 )
 
+const contentTypeOctetStream = "application/octet-stream"
+
+var nowFn = time.Now
+
 // File represents a response that is a response body. The runner is in charge
-// of getting the contents to the destination. The metadata will be received.
+// of getting the contents to the destination. The metadata will be received. One
+// note, we call NormalizeFile on the File's in the Runner's that execute the handler.
+// Testing through the Run function will illustrate this.
 type File struct {
 	ContentType string        `json:"content_type"`
 	Encoding    string        `json:"encoding"`
@@ -23,6 +32,20 @@ type File struct {
 func (f File) MarshalJSON() ([]byte, error) {
 	type alias File
 	return json.Marshal(alias(f))
+}
+
+// NormalizeFile normalizes a file so that all fields are set with sane defaults.
+func NormalizeFile(f File) File {
+	if f.ContentType == "" {
+		f.ContentType = normalizeContentType(f.Filename)
+	}
+	if f.Encoding == "" {
+		f.Encoding = normalizeEncoding(f.Filename)
+	}
+	if f.Filename == "" {
+		f.Filename = normalizeFilename(f.ContentType, f.Encoding, nowFn())
+	}
+	return f
 }
 
 // CompressGzip compresses a files contents with gzip compression.
@@ -98,4 +121,78 @@ func (c *compressorGzip) Close() error {
 	errs = append(errs, c.rc.Close(), c.pr.Close())
 
 	return errors.Join(errs...)
+}
+
+func normalizeContentType(filename string) string {
+	// these mappings here are ones not captured by the mime library at the end.
+	// the jsonnet
+	mappings := [][2]string{
+		{".jsonld", "application/ld+json"},
+		{".json.gz", "application/json"},
+		{".jsonnet", "application/jsonnet"},
+		{".yaml", "text/yaml"},
+		{".yml", "text/yaml"},
+	}
+	for _, pair := range mappings {
+		ext, ct := pair[0], pair[1]
+		if strings.Contains(filename, ext) {
+			return ct
+		}
+	}
+
+	ct := mime.TypeByExtension(filepath.Ext(filename))
+	if ct == "" {
+		ct = contentTypeOctetStream
+	}
+	return ct
+}
+
+func normalizeEncoding(filename string) string {
+	parts := strings.SplitN(filepath.Base(filename), ".", 2)
+	if len(parts) == 1 {
+		return ""
+	}
+
+	mapping := map[string]string{
+		"br":  "brotli",
+		"gz":  "gzip",
+		"zst": "zstd",
+	}
+	var out []string
+	for _, part := range strings.Split(parts[1], ".") {
+		if encoding, ok := mapping[part]; ok {
+			out = append(out, encoding)
+		}
+	}
+	return strings.Join(out, ", ")
+}
+
+var compressionToExt = map[string]string{
+	"brotli": "br",
+	"gzip":   "gz",
+	"zstd":   "zst",
+}
+
+func normalizeFilename(contentType, encoding string, now time.Time) string {
+	filename := "upload_" + now.Format(time.RFC3339)
+	if encoding != "" {
+		var converted []string
+		for _, enc := range strings.Split(strings.ReplaceAll(encoding, " ", ""), ",") {
+			if ext := compressionToExt[enc]; ext != "" {
+				converted = append(converted, ext)
+			}
+		}
+		if len(converted) > 0 {
+			encoding = "." + strings.Join(converted, ".")
+		}
+	}
+
+	var ctExt string
+	if contentType != contentTypeOctetStream {
+		exts, _ := mime.ExtensionsByType(contentType)
+		if len(exts) > 0 {
+			ctExt = exts[0]
+		}
+	}
+	return filename + ctExt + encoding
 }
